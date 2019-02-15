68fccbe5746c738c32c02e6512aaa18000b8104b
Victoria B <viktorija.bourassa@gmail.com>
2019-02-15 19:52:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,14 +21,8 @@ class Student
   end
 
   def self.drop_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students(
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
-
+    sql = <<-SQL DROP TABLE students;
+      SQL
       DB[:conn].execute(sql)
     end
 
